ec73fb37e3d44c77d562722c8aa6720beda0cd6d
Jake Durante <jakeezz15@gmail.com>
2024-04-23 16:10:45 +0300

    Fixes autocomplete and map refresh

--- src/pages/violation/violations-page.jsx
+++ src/pages/violation/violations-page.jsx
@@ -1,51 +1,54 @@
-import { Card, Typography } from '@material-tailwind/react';
-import { useQuery } from '@apollo/client';
-import { GET_VIOLATIONS } from '@/graphql/queries';
-import { useEffect, useState } from 'react';
-import Table from '@/components/Table/Table';
-import LoadingScreen from '@/components/LoadingScreen/LoadingScreen';
-import NavBar from '@/components/NavBar/NavBar';
-import routes from '@/routes';
-import CardHeader from '@/components/CardHeader';
-import Table2 from '@/components/Table/Table2';
-import ViolationsTable from '@/components/Table/ViolationsTable';
+import { Card, Typography } from "@material-tailwind/react";
+import { useQuery } from "@apollo/client";
+import { GET_VIOLATIONS } from "@/graphql/queries";
+import { useEffect, useState } from "react";
+import Table from "@/components/Table/Table";
+import LoadingScreen from "@/components/LoadingScreen/LoadingScreen";
+import NavBar from "@/components/NavBar/NavBar";
+import routes from "@/routes";
+import CardHeader from "@/components/CardHeader";
+import Table2 from "@/components/Table/Table2";
+import ViolationsTable from "@/components/Table/ViolationsTable";
+import { useAuth } from "@/context/AuthContext";
 
 export function ViolationsPage() {
+  const { state } = useAuth();
+  console.log(state);
   const { loading, error, data } = useQuery(GET_VIOLATIONS);
   const [violations, setViolations] = useState();
   const [tableHead, setTableHead] = useState();
   const filterTableHeads = [
-    '__typename',
-    'id',
-    'vehicleId',
-    'violationTypeId',
-    'officerId',
+    "__typename",
+    "id",
+    "vehicleId",
+    "violationTypeId",
+    "officerId",
   ];
   const tHeaders = [
     // 'violationType',
-    'violationName',
-    'description',
-    'plateNumber',
-    'status',
+    "violationName",
+    "description",
+    "plateNumber",
+    "status",
     // 'officer',
-    'timestamp',
+    "timestamp",
   ];
 
   useEffect(() => {
     if (data) {
       setViolations(data.violations);
       setTableHead(
-        data && data.violations.length !== 0 && Object.keys(data.violations[0])
+        data && data.violations.length !== 0 && Object.keys(data.violations[0]),
       );
     }
   }, [data]);
 
   return (
-    <div className="justify-evenly m-0 px-4 flex-wrap md:flex-nowrap overflow-y-auto border-l pl-4">
+    <div className="m-0 flex-wrap justify-evenly overflow-y-auto border-l px-4 pl-4 md:flex-nowrap">
       <div className="w-full ">
-        <CardHeader title={'Violations'} />
+        <CardHeader title={"Violations"} />
         {loading ? (
-          <div className="flex items-center justify-center h-screen">
+          <div className="flex h-screen items-center justify-center">
             <LoadingScreen size={5} color="blue" className="mr-2" />
             <span>Loading...</span>
           </div>
@@ -60,7 +63,7 @@ export function ViolationsPage() {
               data={violations}
               filterKeysValues={filterTableHeads}
               itemsPerPage={6}
-              customCell={'status'}
+              customCell={"status"}
             />
           </div>
         )}
